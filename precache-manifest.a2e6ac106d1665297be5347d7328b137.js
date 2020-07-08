4a9a6971fa608faf833857cff1c5b05b162eaa89
giorgoslytos <giorgoslytos@gmail.com>
2020-07-08 19:39:42 +0300

    Updates

---
+++ precache-manifest.a2e6ac106d1665297be5347d7328b137.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4141dc66fe461ba71c94dc9b47c80a63",
+    "url": "/react-footwear-shop/index.html"
+  },
+  {
+    "revision": "905e05a29186f3ecea90",
+    "url": "/react-footwear-shop/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "12fe04b60306c391f1ca",
+    "url": "/react-footwear-shop/static/css/main.8010212c.chunk.css"
+  },
+  {
+    "revision": "905e05a29186f3ecea90",
+    "url": "/react-footwear-shop/static/js/2.9fe3880e.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/react-footwear-shop/static/js/2.9fe3880e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12fe04b60306c391f1ca",
+    "url": "/react-footwear-shop/static/js/main.8c6896d7.chunk.js"
+  },
+  {
+    "revision": "c7d88e8753ccdf91f328",
+    "url": "/react-footwear-shop/static/js/runtime-main.d270d492.js"
+  },
+  {
+    "revision": "688985cc63c7d54e3b8bd16abbf699ce",
+    "url": "/react-footwear-shop/static/media/Allen-Edmonds-Blind-Brogue-2-1030x646.688985cc.jpg"
+  },
+  {
+    "revision": "ddd81477b1885169df2517d3be7ff698",
+    "url": "/react-footwear-shop/static/media/Dalton_3512_BrownCordovan_Angle_web.ddd81477.jpg"
+  },
+  {
+    "revision": "fc9dd0fc24de3d70c617c6c8aad69564",
+    "url": "/react-footwear-shop/static/media/Foster-and-Son-1030x687.fc9dd0fc.jpg"
+  },
+  {
+    "revision": "5c215141b9401ea2f341fe519737bb48",
+    "url": "/react-footwear-shop/static/media/Hamilton_2825_Brown_Angle_web.5c215141.jpg"
+  },
+  {
+    "revision": "2a2768bdb899dab0a4f98285e37cd0b2",
+    "url": "/react-footwear-shop/static/media/chelsea-brown.2a2768bd.jpg"
+  },
+  {
+    "revision": "b0a58b144234ca4969cb79e9ebe4563f",
+    "url": "/react-footwear-shop/static/media/chelsea-stone.b0a58b14.jpg"
+  },
+  {
+    "revision": "44c1224fa2b55e084823ec67547cb95e",
+    "url": "/react-footwear-shop/static/media/chukka-brown.44c1224f.jpg"
+  },
+  {
+    "revision": "f80e767096d75cb60b7948f3a9c92633",
+    "url": "/react-footwear-shop/static/media/chukka-geniune-brown.f80e7670.jpg"
+  },
+  {
+    "revision": "271bf1b4146a603bf20e92a5ee6944b1",
+    "url": "/react-footwear-shop/static/media/dalton-0116-darkchili-angle-web.271bf1b4.jpg"
+  },
+  {
+    "revision": "af620ac250e1fa17421580f3d98cf474",
+    "url": "/react-footwear-shop/static/media/derbies-cuir-daim-marron-1-1440x1440.af620ac2.jpg"
+  },
+  {
+    "revision": "bcf7ec0670abcc46f7288d97857d5428",
+    "url": "/react-footwear-shop/static/media/derbies-suede.bcf7ec06.jpg"
+  },
+  {
+    "revision": "8101eccc73bb8fe5b0095fb2822eb5fb",
+    "url": "/react-footwear-shop/static/media/desert-midbrown.8101eccc.jpg"
+  },
+  {
+    "revision": "08b281510d8106ba651867c58f29ccf1",
+    "url": "/react-footwear-shop/static/media/full-brogue-shoe.08b28151.jpg"
+  },
+  {
+    "revision": "70e78182cabf15503783f5093f341aae",
+    "url": "/react-footwear-shop/static/media/half-brogue.70e78182.jpg"
+  },
+  {
+    "revision": "c4cd795a065967bde653d418e7cc7622",
+    "url": "/react-footwear-shop/static/media/loafers-brown.c4cd795a.jpg"
+  },
+  {
+    "revision": "3bd76f6b3d55d59a6f86cddca8578898",
+    "url": "/react-footwear-shop/static/media/longwing.3bd76f6b.jpg"
+  },
+  {
+    "revision": "69e014156c51727ab836136657de7ca5",
+    "url": "/react-footwear-shop/static/media/medallion-brogues.69e01415.jpg"
+  },
+  {
+    "revision": "2ec326e6dc2d7b6c9974e32992f6d8a7",
+    "url": "/react-footwear-shop/static/media/moccassin-sebago.2ec326e6.jpg"
+  },
+  {
+    "revision": "4f3b5b41e7f14ad6e7ddc3dad2b1fa31",
+    "url": "/react-footwear-shop/static/media/quarter-brogue.4f3b5b41.jpg"
+  },
+  {
+    "revision": "1f0baa1faea7faa0e64d6d5b653787ae",
+    "url": "/react-footwear-shop/static/media/shoepassion_516-base.1f0baa1f.jpg"
+  },
+  {
+    "revision": "fcd41324f50bf7cfcd47e9e87ab1fcac",
+    "url": "/react-footwear-shop/static/media/williamsburg-2799-black-angle-web.fcd41324.jpg"
+  }
+]);
